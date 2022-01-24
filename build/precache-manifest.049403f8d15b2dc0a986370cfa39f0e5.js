18239440aa2cb8853cdac7cd442b0e7bcbc88456
fadzrinmadu <muhfadzrinmadu@gmail.com>
2022-01-25 06:04:40 +0700

    finished project

---
+++ build/precache-manifest.049403f8d15b2dc0a986370cfa39f0e5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46456b474829480a4dcf27ddbc44be2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe21d3d77b97461ebdca",
+    "url": "/static/css/2.33d22cee.chunk.css"
+  },
+  {
+    "revision": "c9df5cb79514a84b9737",
+    "url": "/static/css/main.2a50e7bd.chunk.css"
+  },
+  {
+    "revision": "fe21d3d77b97461ebdca",
+    "url": "/static/js/2.45aadecc.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.45aadecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9df5cb79514a84b9737",
+    "url": "/static/js/main.79e08a58.chunk.js"
+  },
+  {
+    "revision": "7e11fd638c9697cebc29",
+    "url": "/static/js/runtime-main.d3e53417.js"
+  },
+  {
+    "revision": "b029ba5f920b57b9f3c575d216119e5c",
+    "url": "/static/media/completed.b029ba5f.png"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "e6a7cd81da293d9f8f30d5060caedef3",
+    "url": "/static/media/icon-cities.e6a7cd81.svg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/static/media/icon-star.11a62c71.svg"
+  },
+  {
+    "revision": "d2336c65cbd203779b70abe8b8de0068",
+    "url": "/static/media/icon-traveler.d2336c65.svg"
+  },
+  {
+    "revision": "4f579769a2d277044e739404a945e74f",
+    "url": "/static/media/icon-treasure.4f579769.svg"
+  },
+  {
+    "revision": "3f284a811f42267b0c1133b719ec3512",
+    "url": "/static/media/img-hero.3f284a81.jpg"
+  },
+  {
+    "revision": "82a2a8d46cd3200e858ac23e68dcd3e8",
+    "url": "/static/media/logo-bca.82a2a8d4.png"
+  },
+  {
+    "revision": "865aa86d39d10a614ad0cdacfeb56eec",
+    "url": "/static/media/logo-mandiri.865aa86d.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route exact path="/checkout" component={Checkout} />
+      </Router>
+
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
