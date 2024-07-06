9de8158d1bc44cf2df56cd5c0c4e510df6bf3159
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2024-07-06 22:02:51 +0700

    init basic colors and font tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2749DE",
+        navy: "#152C5B",
+        gray: "#969696",
+        pink: "#FF498B",
+        teal: "#1ABC9C",
+        red: "#E74C3C",
+        white: "#F5F6F8",
+        darkgray: "#898989",
+        lightgray: "#E5E5E5",
+        mediumgray: "#B2B2B2",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
