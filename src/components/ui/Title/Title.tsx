225e6dedb6560819473ca579395173f7f03d00c8
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2025-09-21 08:24:20 +0700

    feat: update title component

--- src/components/ui/Title/Title.tsx
+++ src/components/ui/Title/Title.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import clsx from "clsx";
 
-interface TitleProps {
+interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   text: String;
 }
 
 const Title: React.FC<TitleProps> = (props: TitleProps) => {
-  const { text } = props;
+  const { text, className, ...rest } = props;
   return (
-    <h1 className="text-[42px] font-bold text-[#152C5B]">{text}</h1>
+    <h1 
+      className={clsx("text-[42px] font-bold text-[#152C5B]", className)} 
+      {...rest}
+    >
+      {text}
+    </h1>
   );
 };
 
