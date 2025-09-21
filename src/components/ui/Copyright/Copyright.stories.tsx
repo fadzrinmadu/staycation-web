f5f08b8e4fc521add51e7149c33cf01b77f6e348
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2025-09-21 18:29:00 +0700

    feat: create copyright component

---
+++ src/components/ui/Copyright/Copyright.stories.tsx
@@ -0,0 +1,8 @@
+import { Copyright } from "./Copyright";
+
+export default {
+  title: "Components/UI/Copyright",
+  component: Copyright,
+};
+
+export const Default = () => <Copyright />;
---
+++ src/components/ui/Copyright/Copyright.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Copyright: React.FC = () => {
+  return (
+    <p className="font-light text-base text-[#969696]">Copyright 2020 • All rights reserved • Staycation</p>
+  )
+};
+
+export { Copyright };
