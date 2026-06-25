15581795d91503967c2724bc9d321d89886f34b7
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2026-06-25 14:03:29 +0700

    feat: add navbar variant component

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -7,9 +7,13 @@ import { Logo } from "@/components/ui";
 import { MenuIcon } from "@/assets/images/icons";
 import { menus } from "./menu";
 
-interface NavbarProps {}
+interface NavbarProps {
+  variant?: "centered" | "default";
+}
 
 const Navbar: React.FC<NavbarProps> = (props: NavbarProps) => {
+  const { variant } = props;
+  
   const [toggleMenu, setToggleMenu] = useState(false);
   const [activeId, setActiveId] = useState("home");
   
@@ -24,45 +28,54 @@ const Navbar: React.FC<NavbarProps> = (props: NavbarProps) => {
   }
   
   return (
-    <div className={clsx("relative h-20 py-7 flex justify-between items-center")}>
+    <div 
+      className={clsx(
+        "relative h-20 py-7 flex items-center", 
+        variant === "default" ? "justify-between" : "justify-center"
+      )}
+    >
       <Logo 
         onClick={() => {}} 
       />
-      <button 
-        onClick={handleToggleMenu} 
-        aria-expanded={toggleMenu}
-        className={clsx("w-12 h-12 flex justify-end items-center cursor-pointer block md:hidden")}
-      >
-        <MenuIcon />
-      </button>
-      <ul
-        className={clsx(`
-          absolute md:static top-[88px] left-0 right-0
-          flex flex-col md:flex-row
-          gap-y-3 md:gap-x-12
-          py-4 md:py-0
-          overflow-hidden 
-          bg-gray-100 md:bg-transparent 
-          text-center 
-          transition-all duration-300 ease-in-out`,
-          toggleMenu ? "min-h-[260px] md:min-h-auto opacity-100" : "min-h-0 md:min-h-auto opacity-0 md:opacity-100"
-        )}
-      >
-        {menus.map((menu) => (
-          <li key={menu.id}>
-            <a
-              href={menu.href}
-              onClick={(e) => handleSetActive(e, menu.id)}
-              className={clsx(
-                "py-3 block transition-colors",
-                activeId === menu.id ? "text-[#2749DE]" : "text-[#152C5B]"
-              )}
-            >
-              {menu.name}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {variant === "default" && (
+        <>
+          <button 
+            onClick={handleToggleMenu} 
+            aria-expanded={toggleMenu}
+            className={clsx("w-12 h-12 flex justify-end items-center cursor-pointer block md:hidden")}
+          >
+            <MenuIcon />
+          </button>
+          <ul
+            className={clsx(`
+              absolute md:static top-[88px] left-0 right-0
+              flex flex-col md:flex-row
+              gap-y-3 md:gap-x-12
+              py-4 md:py-0
+              overflow-hidden 
+              bg-gray-100 md:bg-transparent 
+              text-center 
+              transition-all duration-300 ease-in-out`,
+              toggleMenu ? "min-h-[260px] md:min-h-auto opacity-100" : "min-h-0 md:min-h-auto opacity-0 md:opacity-100"
+            )}
+          >
+            {menus.map((menu) => (
+              <li key={menu.id}>
+                <a
+                  href={menu.href}
+                  onClick={(e) => handleSetActive(e, menu.id)}
+                  className={clsx(
+                    "py-3 block transition-colors",
+                    activeId === menu.id ? "text-[#2749DE]" : "text-[#152C5B]"
+                  )}
+                >
+                  {menu.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
